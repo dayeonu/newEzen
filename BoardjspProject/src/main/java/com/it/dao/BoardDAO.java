f8b7d5ac9de7a15dff137a341c6c9c47dc3d2a93
gitTest <88dani@naver.com>
2022-06-07 16:34:04 +0900

    0607

--- BoardjspProject/src/main/java/com/it/dao/BoardDAO.java
+++ BoardjspProject/src/main/java/com/it/dao/BoardDAO.java
@@ -1,110 +1,264 @@
 package com.it.dao;
-import java.util.*; //리스트 가져올 때 사용
-import java.sql.*; // 커넥션 사용할 때 사용 // 서버연결용도 
+
+//Connection.PrepdaedStatment, ResultSet
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+//List
+import java.util.ArrayList;
+import java.util.List;
 
 public class BoardDAO {
-	private Connection conn = null;
-	private PreparedStatement ps = null;
-
-	private final String URL = "jdbc:mysql://localhost:3306/db0416?serverTimezone=UTC";
-	private final String USERNAME = "root";
-	private final String PASSWORD = "1234";
-
-	//드라이브 등록
-	public BoardDAO() {
-		try {
-
-			Class.forName("com.mysql.cj.jdbc.Driver");//드라이브 메모리 할당 
-		}catch(Exception e) {
-			e.printStackTrace();
-		}
-	}
-
-	//MySql 연결 
-	public void getConnection() {
-		try {
-			conn = DriverManager.getConnection(URL,USERNAME, PASSWORD);
-		}catch(Exception e) {
-			e.printStackTrace();
-		}
-	}
-	//MySql 연결끊기
-	public void disConnection() {
-		try {
-			if(ps != null)ps.close();
-			if(conn != null)conn.close();
+   private Connection conn=null;
+   private PreparedStatement ps = null;
+   
+   private final String URL = "jdbc:mysql://localhost:3306/db0416?serverTimezone=UTC";
+   private final String USERNAME = "root";
+   private final String PASSWORD = "1234";
+   
+   
+   //드라이브 등록
+   public BoardDAO() {
+      try {
+         
+         Class.forName("com.mysql.cj.jdbc.Driver"); //드라이브 메모리 활당
+      }catch(Exception e) {
+         e.printStackTrace();
+      }
+   }
+   
+   //MySql 연결
+   public void getConnection() {
+      try {
+         
+         conn = DriverManager.getConnection(URL,USERNAME,PASSWORD);
+      }catch(Exception e) {
+         e.printStackTrace();
+      }
+   }
+   
+   //MySql해제
+   public void disConnection() {
+      try {
+         if(ps != null) ps.close();
+         if(conn != null) conn.close();
+      }catch(Exception e) {
+         e.printStackTrace();
+      }
+   }
+   
+   
+   //1. 목록출력
+    // 게시물 정보 한개 : BoardVO, 게시물 여러개 : (List)
+   public List<BoardVO> boardListData(int page){
+      List<BoardVO> list = new ArrayList<BoardVO>();
+      
+      try {
+         
+         //1. 연결
+         getConnection();
+         //2. SQL문장 작성
+         int rowSize = 10;
+         int start = (rowSize*page) - rowSize; // 10*1 - 10 = 0..  
+         // 10*1 - 10 = 0,10 ..... 10*2-10=10,10, 10*3-10=20,10....
+         String sql = "select no, subject, name, DATE_FORMAT(regdate, '%Y-%m-%d'), hit "  //여기 한칸
+                  + "from jspBoard order by no desc limit ?, ?";
+         
+         //3. 전송
+         ps = conn.prepareStatement(sql);
+         //4. 데이타 채우기
+         ps.setInt(1, start);
+         ps.setInt(2, rowSize);
+         
+         //5. 데이타 실행
+         ResultSet rs = ps.executeQuery();
+         
+         while(rs.next()) {
+            BoardVO vo = new BoardVO();
+           
+            vo.setNo(rs.getInt(1));
+            vo.setSubject(rs.getString(2));
+            vo.setName(rs.getString(3));
+            vo.setDbday(rs.getString(4));
+            vo.setHit(rs.getInt(5));
+            list.add(vo);
+         }
+         //6. 닫기
+         rs.close();
+      }catch(Exception e) {
+         e.printStackTrace();
+      }finally {
+         disConnection();
+      }
+      return list;
+   }
+   
+   
+   //2. 총페이지()
+   public int boardTotalPage() {
+      int total=0;
+      
+      try {
+         
+         getConnection();
+         String sql = "select ceil(count(*)/10.0) from jspBoard"; //CEIL(올림하고자 하는 소수)
+         
+         ps = conn.prepareStatement(sql);
+         ResultSet rs = ps.executeQuery();
+         
+         if(rs.next()) {
+            total = rs.getInt(1);
+         }
+         rs.close();
+         
+      }catch(Exception e) {
+         e.printStackTrace();
+      }finally {
+         disConnection();
+      }
+      
+      return total;
+   }
+   
+   //3. Insert(새글 입력)
+   public void boardInsert(BoardVO vo) {
+	   try {
+		   //1. 연결 
+		   getConnection();
+		   //2. sql 문장 작성 
+		   String sql = "insert into jspBoard(name, subject, content, pwd, regdate) " 
+		   +"values(?,?,?,?,now())"; // now() 오늘날짜
+		
+		   ps = conn.prepareStatement(sql);
+		
+		   //3. 빈칸 채우기
+		   ps.setString(1, vo.getName());
+			ps.setString(2, vo.getSubject());
+			ps.setString(3, vo.getContent());
+			ps.setString(4, vo.getPwd());
+			
+		//4. 실행
+			ps.executeUpdate();
 			
 		}catch(Exception e) {
 			e.printStackTrace();
+		} finally {
+			disConnection();
+			}
 		}
-	}
 
-	//1.목록출력
-	//게시물 정보 한개: BoardVO, 게시물 여러개: (List)
-
-	public List<BoardVO> boardListData(int page){
-		List<BoardVO> list = new ArrayList<BoardVO>();
+   //4. 상세보기(조회수 담당)
+	public BoardVO boardDetailDate(int no) {
+		BoardVO vo = new BoardVO();
 		
 		try {
-			
-			//1. 연결
+			//1.연결
 			getConnection();
-			//2. SQL문장 작성
-			int rowSize = 10;
-			int start = (rowSize*page) - rowSize; // 페이지 시작 부터 10개씩 출력 
-			String sql = "select no, subject, name, DATE_FORMAT(regdate, '%Y-%m-%d'),hit"
-					+ "from jspBoard order by no desc limit ?, ?";
+			 String sql = "update jspBoard set hit = hit +1 where no = ?";
 			
-			//3. 전송
-			ps = conn.prepareStatement(sql);
-			//4. 데이터 채우기
-			ps.setInt(1, start);
-			ps.setInt(2, rowSize);
+			ps= conn.prepareStatement(sql);
+			ps.setInt(1, no);
+			ps.executeUpdate();
 			
-			 //5. 데이터 실행 
-			ResultSet rs = ps.executeQuery();
+			 sql = "select no , name, subject, content, DATE_FORMAT(regdate, '%Y-%m-%d'), hit from jspBoard where no = ?";
 			
-			while(rs.next()) {
-				BoardVO vo = new BoardVO();
+			 ps= conn.prepareStatement(sql);
+			 ps.setInt(1, no);
+			 ResultSet rs = ps.executeQuery();
+			 
+			if(rs.next()) {
 				vo.setNo(rs.getInt(1));
-				vo.setSubject(rs.getString(2));
-				vo.setName(rs.getString(3));
-				vo.setDbday(rs.getString(4));
-				vo.setHit(rs.getShort(5));
-				list.add(vo);
+				vo.setName(rs.getString(2));
+				vo.setSubject(rs.getString(3));
+				vo.setContent(rs.getString(4));
+				vo.setDbday(rs.getString(5));
+				vo.setHit(rs.getInt(6));
+				rs.close();
 			}
-			//6. 닫기
-			rs.close();
 		}catch(Exception e) {
 			e.printStackTrace();
 		}finally {
 			disConnection();
-		} 
-		
-		return list;
+		}
+		return vo;
+	}
+
+	//5. 수정하기
+	public BoardVO boardUpdatedata(int no) {
+		BoardVO vo = new BoardVO();
+		try {
+			//1.연결
+			getConnection();
+			
+			String sql = "select no, name, subject, content, DATE_FORMAT(regdate, '%Y-%m-%d'), hit "
+					+ "from jspBoard where no=?";
+			ps= conn.prepareStatement(sql);
+			ps.setInt(1, no);
+			ResultSet rs = ps.executeQuery();
+			
+			if(rs.next()) {
+				vo.setNo(rs.getInt(1));
+				vo.setName(rs.getString(2));
+				vo.setSubject(rs.getString(3));
+				vo.setContent(rs.getString(4));
+				vo.setDbday(rs.getString(5));
+				vo.setHit(rs.getInt(6));
+				rs.close();
+			}
+		}catch(Exception e) {
+			e.printStackTrace();
+		}finally {
+			disConnection();
+		}
+		return vo;
 	}
 	
-	//2. 홈페이지()
-	public int boardTotalPage() {
-		int total = 0;
+	
+	public boolean boardUpdata(BoardVO vo) {
+	
+		boolean chk = false;
+
+	try {
+		getConnection();
 		
-			try {
-				getConnection();
-				String sql="select cell(count(*)/10.0 from jspBoard";
+		String sql = "select pwd from jspBoard where no=?";
+
+		ps= conn.prepareStatement(sql);
+		ps.setInt(1, vo.getNo());
+		ResultSet rs = ps.executeQuery();
+		
+		String chk_pwd="";
+		if(rs.next()) 
+			chk_pwd= rs.getString(1);
+			rs.close();
+			
+			if(chk_pwd.equals(vo.getPwd())) {
+				chk=true;
 				
+				sql = "update jspBoard set name=?, subject=?, content=?, where no=?";
+			
 				ps = conn.prepareStatement(sql);
-				ResultSet rs = ps.executeQuery();
-				
-				if(rs.next()) {
-					total = rs.getInt(1);
-				}
-				rs.close();
-				
-			}catch(Exception e) {
-				e.printStackTrace();
-			}finally {
-				disConnection();
+				ps.setString(1, vo.getName());
+				ps.setString(2, vo.getSubject());
+				ps.setString(3, vo.getContent());
+				ps.setInt(4, vo.getNo());
+				ps.executeUpdate();
 			}
-		return total;
+		}catch(Exception e) {
+			e.printStackTrace();
+		}finally {
+			disConnection();
+		}
+		return chk;
 	}
 }
+   
+   
+   
+   
+   
+   
+   
+   
+   
